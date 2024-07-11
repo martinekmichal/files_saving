36aa8cde71d19c4c0dfc63195cd82a36dbd64de4
martinekmichal <martinek.m.84@gmail.com>
2024-07-11 18:17:03 +0200

    vytvoreni souboru + zapis do souboru

---
+++ task1_text_file.py
@@ -0,0 +1,8 @@
+nazev = input("Zadej nazev souboru s příponou:")
+text = input(" Zadej text do souboru:")
+
+with open(nazev, "w", encoding="utf-8") as soubor:
+    soubor.write(text)
+
+print(f"Text byl uložen do souboru {nazev}")
+
